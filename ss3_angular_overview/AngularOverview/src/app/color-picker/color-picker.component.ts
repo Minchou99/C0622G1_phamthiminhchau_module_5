c35186845e1b0900488656ce1df5458c052afe10
Minchou99 <minhchaupt2903@gmail.com>
2022-11-22 11:13:19 +0700

    update

---
+++ ss3_angular_overview/AngularOverview/src/app/color-picker/color-picker.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-color-picker',
+  templateUrl: './color-picker.component.html',
+  styleUrls: ['./color-picker.component.css']
+})
+export class ColorPickerComponent implements OnInit {
+  red = '#FF0000';
+  blue = '#0000FF';
+  yellow = '#FFFF00';
+  color = 'red';
+  colorChange: string;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  change(s: string) {
+    this.color = s;
+  }
+
+}
